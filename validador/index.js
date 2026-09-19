9a2292e0889d52606aa8d57c799b41957a6d09be
prietocoin <86864490+prietocoin@users.noreply.github.com>
2026-09-19 07:28:58 -0400

    Update index.js

--- validador/index.js
+++ validador/index.js
@@ -15,7 +15,9 @@ const pool = new Pool({
 
 const redisConnection = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
 });
 
 const s3Client = new S3Client({
