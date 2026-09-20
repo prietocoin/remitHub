8e2f1c213100106119c7a361546c30ae08da8626
prietocoin <86864490+prietocoin@users.noreply.github.com>
2026-09-20 10:46:49 -0400

    Update server startup message and remove worker comment

--- ingesta/index.js
+++ ingesta/index.js
@@ -1,8 +1,7 @@
 const app = require('./server');
-// Iniciar la escucha del worker en segundo plano
-require('./src/workers/download.worker');
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`[Ingesta] 🟢 Servidor HTTP y Worker activos en el puerto ${PORT}`);
+  console.log(`[Ingesta] 🟢 Servidor HTTP de Ingesta Ciega activo en el puerto ${PORT}`);
 });
