5f4baa31001c108fc01f5fdfb6306776f8718d72
prietocoin <86864490+prietocoin@users.noreply.github.com>
2026-09-23 15:48:41 -0400

    Delete ingesta/index.js

--- ingesta/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./server');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`[Ingesta] 🟢 Servidor HTTP de Ingesta Ciega activo en el puerto ${PORT}`);
-});
