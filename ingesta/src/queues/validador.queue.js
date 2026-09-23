dcd418a788da30e852638bf63b1bb30dfa4b514c
prietocoin <86864490+prietocoin@users.noreply.github.com>
2026-09-23 15:20:06 -0400

    Update validador.queue.js

--- ingesta/src/queues/validador.queue.js
+++ ingesta/src/queues/validador.queue.js
@@ -1,13 +1,1 @@
-const { Queue } = require('bullmq');
-const redisConfig = require('../config/redis');
 
-const validadorQueue = new Queue('cola-validador', {
-  connection: redisConfig,
-  defaultJobOptions: {
-    attempts: 3,
-    backoff: { type: 'exponential', delay: 1000 },
-    removeOnComplete: true,
-  }
-});
-
-module.exports = validadorQueue;
